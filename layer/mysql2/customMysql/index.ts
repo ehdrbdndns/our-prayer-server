46afe95ff9c66b5d549f39e9a5a14264421de4af
ehdrbdndns <ehdrbdndns@naver.com>
2024-12-14 15:08:00 +0900

    feat: add utility functions for date formatting, unique ID generation, and XSS filtering

--- layer/mysql2/customMysql/index.ts
+++ layer/mysql2/customMysql/index.ts
@@ -1,6 +1,26 @@
 import { createPool } from 'mysql2';
 import { DB_DATABASE, DB_HOST, DB_PASSWORD, DB_USER } from './keys';
 
+export function getFullDateFromDate(date: Date) {
+  return `${date.getFullYear()}-${date.getMonth()}-${date.getDate()}`
+}
+
+export function getFullTimeFromDate(date: Date) {
+  return `${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}`
+}
+
+export function getFullDateTimeFromDate(date: Date) {
+  return `${getFullDateFromDate(date)} ${getFullTimeFromDate(date)}`
+}
+
+export function createUniqId() {
+  return Date.now().toString(36) + Math.random().toString(36).substr(2);
+}
+
+export function XSSFilter(content: string) {
+  return content.replace(/</g, "<").replace(/>/g, ">");
+}
+
 // MySQL 연결 풀 생성
 export const pool = createPool({
   host: DB_HOST,
