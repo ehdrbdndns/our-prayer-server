2f4c1221ca1e94ba34c27cf77eaba210979c67f3
ehdrbdndns <ehdrbdndns@naver.com>
2026-01-23 14:31:19 +0900

    feat: add app notice handler and integrate with router

---
+++ function/appInfo/appNotice.ts
@@ -0,0 +1,80 @@
+import { APIGatewayEvent, APIGatewayProxyResult, Context } from "aws-lambda";
+
+import { promisePool } from 'customMysql';
+import { AppInfoType } from "../dataType";
+
+async function handleGet(): Promise<APIGatewayProxyResult> {
+
+  try {
+
+    const [rows] = await promisePool.query(`
+    SELECT * FROM app_notice
+    `, []) as [AppInfoType[], unknown];
+
+    if (rows.length === 0) {
+      return {
+        statusCode: 404,
+        headers: {
+          "Access-Control-Allow-Origin": "*",
+        },
+        body: JSON.stringify({ message: "not found" }),
+      };
+    }
+
+    return {
+      statusCode: 200,
+      headers: {
+        "Access-Control-Allow-Origin": "*",
+      },
+      body: JSON.stringify({
+        ...rows[0]
+      }),
+    }
+
+  } catch (e) {
+    console.error(e);
+    return {
+      statusCode: 500,
+      headers: {
+        "Access-Control-Allow-Origin": "*",
+      },
+      body: JSON.stringify({ message: "internal server error" }),
+    };
+  }
+}
+
+export const appNoticeHandler = async (event: APIGatewayEvent, context: Context): Promise<APIGatewayProxyResult> => {
+  try {
+    const HttpMethod = event.requestContext.httpMethod;
+
+    let response: any = {};
+
+    switch (HttpMethod.toLowerCase()) {
+      case "get":
+        response = await handleGet();
+        break;
+      default:
+        response = {
+          statusCode: 405,
+          headers: {
+            "Access-Control-Allow-Origin": "*",
+          },
+          body: JSON.stringify({ message: "Method Not Allowed" }),
+        };
+        break;
+    }
+
+    return response;
+
+  } catch (e) {
+    console.error(e);
+    return {
+      statusCode: 500,
+      headers: {
+        "Access-Control-Allow-Origin": "*",
+      },
+      body: JSON.stringify({ message: "internal server error" }),
+    };
+
+  }
+}
